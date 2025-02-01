f5a3f3cf61117e49e74da3a4969b0117581d50ac
Piyush Rebadiya <piyushrebadiya123@gmail.com>
2025-02-01 12:07:47 +0530

    add to ParentOrganizerUkeyId in Organizer Master

--- src/controllers/Organizer_Master.js
+++ src/controllers/Organizer_Master.js
@@ -31,15 +31,16 @@ const FetchOrganizerDetails = async (req, res)=>{
 }
 
 const OrginazerMaster = async (req, res)=>{
-    const { OrganizerName = '', OrganizerUkeyId = generateUUID(), StateCode = '', StateName = null, Email = null, Mobile1 = null, Mobile2 = null, AliasName = null, Description = null, Add1 = null, Add2 = null, City = null, IsActive = true, UsrID = null, UPI = null, flag = null, UserName = null,
+    const { OrganizerName = '', OrganizerUkeyId = generateUUID(), ParentOrganizerUkeyId = '', StateCode = '', StateName = null, Email = null, Mobile1 = null, Mobile2 = null, AliasName = null, Description = null, Add1 = null, Add2 = null, City = null, IsActive = true, UsrID = null, UPI = null, flag = null, UserName = null,
     Password = null, Role = 'Admin', RazorpayKeyId = null, RazorpaySecretKey = null, RazorpayBusinessName = null } = req.body;
     
     try{
         const {IPAddress, ServerName, EntryTime} = getCommonKeys();
 
         const insertQuery = `
-            INSERT INTO OrganizerMaster (OrganizerUkeyId, OrganizerName, Mobile1, Mobile2, Email, AliasName, Description, Add1, Add2, City, StateCode, StateName, UPI, IsActive, UsrID, IpAddress, HostName, EntryDate, flag, UserName, Password, Role, RazorpayKeyId, RazorpaySecretKey, RazorpayBusinessName) VALUES (
-            ${setSQLStringValue(OrganizerUkeyId)}, 
+            INSERT INTO OrganizerMaster (OrganizerUkeyId, ParentOrganizerUkeyId, OrganizerName, Mobile1, Mobile2, Email, AliasName, Description, Add1, Add2, City, StateCode, StateName, UPI, IsActive, UsrID, IpAddress, HostName, EntryDate, flag, UserName, Password, Role, RazorpayKeyId, RazorpaySecretKey, RazorpayBusinessName) VALUES (
+            ${setSQLStringValue(OrganizerUkeyId)},
+            ${setSQLStringValue(ParentOrganizerUkeyId)}, 
             ${setSQLStringValue(OrganizerName)}, 
             ${setSQLStringValue(Mobile1)}, 
             ${setSQLStringValue(Mobile2)}, 
@@ -116,15 +117,15 @@ const OrginazerMaster = async (req, res)=>{
 
 const RemoveOrginazer = async (req, res) => {
     try{
-        const {OrganizerId} = req.query;
+        const {OrganizerUkeyId} = req.query;
 
-        const missingKeys = checkKeysAndRequireValues(['OrganizerId'], req.query);
+        const missingKeys = checkKeysAndRequireValues(['OrganizerUkeyId'], req.query);
 
         if(missingKeys.length > 0){
             return res.status(400).json(errorMessage(`${missingKeys.join(', ')} is Required`));
         }
 
-        const checkOrganizer = await pool.request().query(`SELECT * FROM OrganizerMaster WHERE OrganizerId = '${OrganizerId}'`);
+        const checkOrganizer = await pool.request().query(`SELECT * FROM OrganizerMaster WHERE OrganizerUkeyId = '${OrganizerUkeyId}'`);
 
         if(checkOrganizer.rowsAffected[0] == 0){
             return res.status(400).json({...errorMessage('No Orginizer Found.')})
@@ -135,16 +136,16 @@ const RemoveOrginazer = async (req, res) => {
         }
 
         const query = `
-            DELETE FROM OrganizerMaster WHERE OrganizerId = '${OrganizerId}' AND Role != 'SuperAdmin' AND Role != 'Admin';
+            DELETE FROM OrganizerMaster WHERE OrganizerUkeyId = '${OrganizerUkeyId}'
         `
-    
+
         const result = await pool.request().query(query);
-            
+
         if(result.rowsAffected[0] === 0){
             return res.status(400).json({...errorMessage('No Orginizer Deleted.')})
         }
 
-        return res.status(200).json({...successMessage('Orginizer Deleted Successfully.'), OrganizerId});
+        return res.status(200).json({...successMessage('Orginizer Deleted Successfully.'), OrganizerUkeyId});
     }catch(error){
         console.log('Delete Event Error :', error);
         return res.status(500).json({...errorMessage(error.message)});
