22d0a2f31b2810d52a390f09179d7faac51b1516
parth chothani <parthchothani560@gmail.com>
2025-02-24 17:40:13 +0530

    Done Orgenizer signup API

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -51,9 +51,8 @@ router.delete("/delete_user_master", auth, UserMasterController.deleteUserMaster
 
 router.post("/verify_user_master", UserMasterController.verifyHandler)
 
-router.get('/user_listr', UserController.FetchUser);
 router.get('/login', UserController.LoginUser);
-router.post('/signup', UserController.AddUser);
+router.post('/orginizer_signup', UserController.AddOrginizer);
 
 router.get('/fetch_event_list', auth, EventMasterController.EventList);
 router.post('/event_master', auth, EventUpload, EventController.addEvent);
--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,94 +1,75 @@
-const { errorMessage, successMessage, checkKeysAndRequireValues, generateCODE, setSQLBooleanValue, getCommonKeys, generateJWTT, generateUUID, setSQLStringValue } = require("../common/main");
+const { errorMessage, successMessage, checkKeysAndRequireValues, generateCODE, setSQLBooleanValue, getCommonKeys, generateJWTT, generateUUID, setSQLStringValue, setSQLNumberValue } = require("../common/main");
 const {pool} = require('../sql/connectToDatabase');
 
-const FetchUser = async (req, res) =>{
+const AddOrginizer = async (req, res) => {
     try{
+        const { OrganizerName, Mobile1, Mobile2, Email, AliasName, Description, Add1, Add2, City, StateCode, StateName, Password = '', Pincode = 0 } = req.body;
 
-    }catch(error){
-        console.log('Fetch User Error :', error);
-        return res.status(500).json({...errorMessage(error.message)});
-    }
-}
-
-const AddUser = async (req, res) => {
-    try{
-        const {OrganizerName, OrganizerMobile, OrganizerEmail, EventName, EventAddress, EventCity, UserName, Password = '', IsActive = true, flag = 'A', TypeofAddress, Address1 = '', Address2 = '', Pincode = '', IsPrimaryAddress, ParentOrganizerUkeyId, Role} = req.body;
-
-        const missingKeys = checkKeysAndRequireValues(['OrganizerName', 'OrganizerMobile', 'OrganizerEmail', 'EventName', 'EventAddress', 'EventCity', 'UserName', 'Password'], req.body);
+        const missingKeys = checkKeysAndRequireValues(['OrganizerName', 'Mobile1', 'Add1', 'City', 'UserName', 'Password', 'Email'], req.body);
 
         if(missingKeys.length > 0){
             return res.status(400).json(errorMessage(`${missingKeys.join(', ')} is Required`));
         }
-        if(!Role) {
-            return res.status(400).json(errorMessage('Role Is required while creating new Organization.'));
-        }else if(Role !== 'Admin' && Role !== 'SubAdmin'){
-            return res.status(400).json(errorMessage("Role can only be 'Admin' or 'SubAdmin'."));
-        }
 
         const OrganizerUKeyId = generateUUID();
         const EventUKeyId = generateUUID();
+        const UserUkeyId = generateUUID();
         const AddressUkeyID = generateUUID();
-        const EventCode = generateCODE(EventName);
+        const EventCode = generateCODE(OrganizerName);
         
-        const {IPAddress, ServerName, EntryTime} = getCommonKeys(); 
+        const {IPAddress, ServerName, EntryTime} = getCommonKeys(req); 
 
-        let resultReg ,resultOrgMst ,resultEvent, resultAddress;
-        if(flag === 'A'){
-            const InsertReg = `
-                INSERT INTO OrganizerRegistration ( 
-                    OrganizerUkeyId, OrganizerName, OrganizerMobile, OrganizerEmail, EventCode, EventName, EventAddress, EventCity, UserName, Password, ServerName, IsActive, IpAddress, EntryDate, flag
-                ) VALUES (
-                    ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(OrganizerName)}, ${setSQLStringValue(OrganizerMobile)}, ${setSQLStringValue(OrganizerEmail)}, ${setSQLStringValue(EventCode)}, ${setSQLStringValue(EventName)}, ${setSQLStringValue(EventAddress)}, ${setSQLStringValue(EventCity)}, ${setSQLStringValue(UserName)}, ${setSQLStringValue(Password)}, ${setSQLStringValue(ServerName)}, ${setSQLBooleanValue(IsActive)}, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(EntryTime)}, 'A'
-                );
-            `
+        const InsertOrgUsrMst = `  
+            INSERT INTO OrganizerMaster ( 
+                OrganizerUkeyId, OrganizerName, Mobile1, Mobile2, Email, AliasName, Description, Add1, Add2, City, StateCode, StateName, IsActive, IpAddress, HostName, EntryDate, flag
+            ) OUTPUT INSERTED.*  VALUES (
+                ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(OrganizerName)}, ${setSQLStringValue(Mobile1)}, ${setSQLStringValue(Mobile2)}, ${setSQLStringValue(Email)}, ${setSQLStringValue(AliasName)}, ${setSQLStringValue(Description)}, ${setSQLStringValue(Add1)}, ${setSQLStringValue(Add2)}, ${setSQLStringValue(City)}, ${setSQLStringValue(StateCode)}, ${setSQLStringValue(StateName)}, 1, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, 'A'
+            );    
+        `;
+    
+        const resultOrgUsrMst = await pool.request().query(InsertOrgUsrMst)
 
-            resultReg = await pool.request().query(InsertReg);
-            
-            const InsertOrgMst = `
-                INSERT INTO OrganizerMaster ( 
-                    OrganizerUkeyId, ParentOrganizerUkeyId, OrganizerName, Mobile1, Email, EntryDate, flag, UserName, Password, IsActive, Role
-                ) VALUES (
-                    ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(ParentOrganizerUkeyId)}, ${setSQLStringValue(OrganizerName)}, ${setSQLStringValue(OrganizerMobile)}, ${setSQLStringValue(OrganizerEmail)}, ${setSQLStringValue(EntryTime)}, 'A', ${setSQLStringValue(UserName)}, ${setSQLStringValue(Password)}, 1, ${setSQLStringValue(Role)}
-                );    
-            `
+        const InsertOrgUserMst = `
+            INSERT INTO OrguserMaster ( 
+                UserUkeyId, EventUKeyId, OrganizerUkeyId, Password, IsActive, IpAddress, HostName, EntryDate, FirstName, Mobile1, Mobile2, StateCode, StateName, CityName, Role, flag
+            ) OUTPUT INSERTED.* VALUES (
+                ${setSQLStringValue(UserUkeyId)}, ${setSQLStringValue(EventUKeyId)}, ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(Password)}  , 1, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, ${setSQLStringValue(OrganizerName)}, ${setSQLStringValue(Mobile1)}, ${setSQLStringValue(Mobile2)}, ${setSQLNumberValue(StateCode)}, ${setSQLStringValue(StateName)}, ${setSQLStringValue(City)}, 'Admin', 'A'
+            );
+        `;
 
-            resultOrgMst = await pool.request().query(InsertOrgMst);
-            
-            const InsertEvent = `
-                INSERT INTO EventMaster ( 
-                    EventUKeyId, OrganizerUkeyId, EventName, EventCode, AddressUkeyID, EntryDate, IsActive
-                ) VALUES (
-                    ${setSQLStringValue(EventUKeyId)}, ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(EventName)}, ${setSQLStringValue(EventCode)}, ${setSQLStringValue(AddressUkeyID)}, ${setSQLStringValue(EntryTime)}, 1
-                );    
-            `
+        const resultOrgUserMst = await pool.request().query(InsertOrgUserMst);
 
-            resultEvent = await pool.request().query(InsertEvent);
+        const InsertAddress = `  
+            INSERT INTO AddressMaster ( 
+                AddressUkeyID, OrganizerUkeyId, EventUkeyId, MobileNumber, Email, Alias, Address1, Address2, CityName, StateCode, StateName, IsActive, IpAddress, HostName, EntryDate, flag, Pincode, CountryName, IsPrimaryAddress, UsreID
+            ) OUTPUT INSERTED.*  VALUES (
+                ${setSQLStringValue(AddressUkeyID)}, ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(EventUKeyId)}, ${setSQLStringValue(Mobile1)}, ${setSQLStringValue(Email)}, ${setSQLStringValue(AliasName)}, ${setSQLStringValue(Add1)}, ${setSQLStringValue(Add2)}, ${setSQLStringValue(City)}, ${setSQLStringValue(StateCode)}, ${setSQLStringValue(StateName)}, 1, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, 'A', ${setSQLNumberValue(Pincode)}, 'INDIA', 1, ${setSQLNumberValue(resultOrgUserMst.recordset[0].UserId)}
+            );    
+        `;
+    
+        const resulAddress = await pool.request().query(InsertAddress)
 
-            const InsertAddress = `
-                INSERT INTO AddressMaster ( 
-                    AddressUkeyID, EventUKeyId, OrganizerUkeyId, TypeofAddress, Address1, Pincode, CityName, IsPrimaryAddress, IsActive, EntryDate
-                ) VALUES (
-                    ${setSQLStringValue(AddressUkeyID)}, ${setSQLStringValue(EventUKeyId)}, ${setSQLStringValue(OrganizerUKeyId)}, ${setSQLStringValue(TypeofAddress)}, ${setSQLStringValue(EventAddress)}, '${Pincode}', ${setSQLStringValue(EventCity)}, ${setSQLBooleanValue(IsPrimaryAddress)}, 1, ${setSQLStringValue(EntryTime)}
-                ); 
-            `
+        const InsertEvent = `
+        INSERT INTO EventMaster ( 
+            EventUKeyId, OrganizerUkeyId, EventName, EventCode, IsActive, IpAddress, HostName, EntryDate, EventDate, UsreID, AddressUkeyID
+        ) OUTPUT INSERTED.* VALUES (
+            ${setSQLStringValue(EventUKeyId)}, ${setSQLStringValue(OrganizerUKeyId)}, 'Default Event', ${setSQLStringValue(EventCode)}, 1, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, GETDATE(), ${resultOrgUserMst.recordset[0].UserId}, ${setSQLStringValue(AddressUkeyID)}
+            );
+        `;
+    
+        const resultEvent = await pool.request().query(InsertEvent);
 
-            resultAddress = await pool.request().query(InsertAddress);
-        } else {
-            return res.status(400).json({...errorMessage('User Not Registerd Successfully.')})
-        }        
-
-        if(resultReg.rowsAffected[0] === 0 && resultOrgMst.rowsAffected[0] === 0 && resultEvent.rowsAffected[0] === 0 && resultAddress.rowsAffected[0] === 0){
+        if(resultOrgUsrMst.rowsAffected[0] === 0 && resultEvent.rowsAffected[0] === 0 && resultOrgUserMst.rowsAffected[0] === 0 && resulAddress.rowsAffected[0] === 0){
             return res.status(400).json({...errorMessage('User Not Registerd Successfully.')})
         }
 
         return res.status(200).json({
             ...successMessage('User Registerd Successfully.'), 
             token : generateJWTT({
-                OrganizerName
-                , OrganizerMobile
-                , UserName
-                , OrganizerUKeyId
-                , ParentOrganizerUkeyId
+                OrganizerUKeyId
+                , EventUKeyId
+                , Role : 'Admin'
+                , UserId : resultOrgUserMst.recordset[0].UserId
             }),
             OrganizerUKeyId,
             ...req.body
@@ -99,14 +80,6 @@ const AddUser = async (req, res) => {
     }
 }
 
-const DeleteUser = async (req, res) => {
-    try{
-
-    }catch(error){
-        console.log('Delete User Error :', error);
-        return res.status(500).json({...successMessage()})
-    }
-}
 
 
 const LoginUser = async (req, res) => {
@@ -146,8 +119,6 @@ const LoginUser = async (req, res) => {
 }
 
 module.exports = {
-    FetchUser,
-    AddUser,
-    DeleteUser,
+    AddOrginizer,
     LoginUser,
 }
