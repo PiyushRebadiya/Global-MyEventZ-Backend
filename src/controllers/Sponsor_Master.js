38ea298fa968c90d823dd0752171b44dac434a29
parth chothani <parthchothani560@gmail.com>
2025-02-27 12:57:05 +0530

    Done CRUD of sponsor master

--- src/controllers/Sponsor_Master.js
+++ src/controllers/Sponsor_Master.js
@@ -3,23 +3,24 @@ const {pool} = require('../sql/connectToDatabase');
 
 const FetchSponsorMasterDetails = async (req, res)=>{
     try{
-        const { SponsorUkeyId, IsActive, LinkType } = req.query;
+        const { SponsorUkeyId, OrganizerUkeyId, EventUkeyId, SponsorCatUkeyId } = req.query;
         let whereConditions = [];
 
-        // Build the WHERE clause based on the Status
         if (SponsorUkeyId) {
-            whereConditions.push(`SponsorUkeyId = '${SponsorUkeyId}'`);
+            whereConditions.push(`SponsorUkeyId = ${setSQLStringValue(SponsorUkeyId)}`);
         }
-        if (LinkType) {
-            whereConditions.push(`LinkType = ${setSQLStringValue(LinkType)}`);
+        if (OrganizerUkeyId) {
+            whereConditions.push(`OrganizerUkeyId = ${setSQLStringValue(OrganizerUkeyId)}`);
         }
-        if(IsActive){
-            whereConditions.push(`IsActive = ${setSQLBooleanValue(IsActive)}`);
+        if (EventUkeyId) {
+            whereConditions.push(`EventUkeyId = ${setSQLStringValue(EventUkeyId)}`);
         }
-        // Combine the WHERE conditions into a single string
+        if (SponsorCatUkeyId) {
+            whereConditions.push(`SponsorCatUkeyId = ${setSQLStringValue(SponsorCatUkeyId)}`);
+        }
         const whereString = whereConditions.length > 0 ? `WHERE ${whereConditions.join(' AND ')}` : '';
         const getUserList = {
-            getQuery: `SELECT * FROM SponsorMaster ${whereString} ORDER BY SponsorId DESC`,
+            getQuery: `SELECT * FROM SponsorMaster ${whereString} ORDER BY EntryDate DESC`,
             countQuery: `SELECT COUNT(*) AS totalCount FROM SponsorMaster ${whereString}`,
         };
         const result = await getCommonAPIResponse(req, res, getUserList);
@@ -32,82 +33,61 @@ const FetchSponsorMasterDetails = async (req, res)=>{
 
 const SponsorMaster = async (req, res) => {
     const { 
-        SponsorUkeyId = generateUUID(), SponsorCatUkeyId = generateUUID(), Name = '', Mobile = '', CompanyName = '', UsrName = '', UsrID = '', flag = '' , OrganizerUkeyId = '',EventUkeyId = '', Description1 = '', Description2 = '', Description3 = '', Description4 = '', Link = '', LinkType = 'WEB'
+        SponsorUkeyId = '', SponsorCatUkeyId = '', Name = '', Mobile = '', CompanyName = '', flag = '' , OrganizerUkeyId = '',EventUkeyId = '', Description1 = '', Description2 = '', Description3 = '', Description4 = ''
     } = req.body;
-    let {Img = ''} = req.body;
-
-    Img = req?.files?.Img?.length ? `${req?.files?.Img[0]?.filename}` : Img;
-
     try {
-        const { IPAddress, ServerName, EntryTime } = getCommonKeys();
+        const { IPAddress, ServerName, EntryTime } = getCommonKeys(req);
 
         const insertQuery = `
             INSERT INTO SponsorMaster (
-                SponsorUkeyId, SponsorCatUkeyId, Name, Mobile, CompanyName, Img, UsrName, UsrID, IpAddress, HostName, EntryDate, flag, OrganizerUkeyId, EventUkeyId,Description1, Description2, Description3, Description4, Link, LinkType, OrganizerId
+                SponsorUkeyId, SponsorCatUkeyId, Name, Mobile, CompanyName, UserName, UserID, IpAddress, HostName, EntryDate, flag, OrganizerUkeyId, EventUkeyId,Description1, Description2, Description3, Description4
             ) VALUES (
-                N'${SponsorUkeyId}', N'${SponsorCatUkeyId}', N'${Name}', N'${Mobile}', N'${CompanyName}', N'${Img}', N'${UsrName}', N'${UsrID}', N'${IPAddress}', N'${ServerName}', N'${EntryTime}', N'${flag}', N'${OrganizerUkeyId}', N'${EventUkeyId}', '${Description1}', '${Description2}', '${Description3}', '${Description4}', ${setSQLStringValue(Link)}, ${setSQLStringValue(LinkType)}, ${setSQLNumberValue(req?.user?.OrganizerId)}
+                ${setSQLStringValue(SponsorUkeyId)}, ${setSQLStringValue(SponsorCatUkeyId)}, ${setSQLStringValue(Name)}, ${setSQLStringValue(Mobile)}, ${setSQLStringValue(CompanyName)}, ${setSQLStringValue(req.user.FirstName)}, ${setSQLStringValue(req.user.UserId)}, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, ${setSQLStringValue(flag)}, ${setSQLStringValue(OrganizerUkeyId)}, ${setSQLStringValue(EventUkeyId)}, ${setSQLStringValue(Description1)}, ${setSQLStringValue(Description2)}, ${setSQLStringValue(Description3)}, ${setSQLStringValue(Description4)}
             );
         `;
 
         const deleteQuery = `
-            DELETE FROM SponsorMaster WHERE SponsorUkeyId = '${SponsorUkeyId}';
+            DELETE FROM SponsorMaster WHERE SponsorUkeyId = ${setSQLStringValue(SponsorUkeyId)};
         `;
 
-        if (flag === 'A') {
-            const missingKeys = checkKeysAndRequireValues(['Img'], { ...req.body, ...req?.files });
+        const missingKeys = checkKeysAndRequireValues(['SponsorUkeyId', 'SponsorCatUkeyId', 'Name', 'OrganizerUkeyId', 'EventUkeyId'], { ...req.body });
 
-            if (missingKeys.length > 0) {
-                if (Img) deleteImage(req?.files?.Img?.[0]?.path); // Only delete if `Img` exists
-                return res.status(400).json(errorMessage(`${missingKeys.join(', ')} is Required`));
-            }
+        if (missingKeys.length > 0) {
+            return res.status(400).json(errorMessage(`${missingKeys.join(', ')} is Required`));
+        }
+        if (flag === 'A') {
 
             const result = await pool.request().query(insertQuery);
 
             if (result?.rowsAffected?.[0] === 0) {
-                if (Img) deleteImage(req?.files?.Img?.[0]?.path); // Only delete if `Img` exists
                 return res.status(400).json({ ...errorMessage('No Sponsor Created.') });
             }
 
             return res.status(200).json({ 
                 ...successMessage('New Sponsor Created Successfully.'), 
-                ...req.body, SponsorUkeyId, Img , SponsorCatUkeyId
+                ...req.body
             });
 
         } else if (flag === 'U') {
-            const missingKeys = checkKeysAndRequireValues(['Img'], { ...req.body, ...req?.files });
-
-            if (missingKeys.length > 0) {
-                if (Img) deleteImage(req?.files?.Img?.[0]?.path); // Only delete if `Img` exists
-                return res.status(400).json(errorMessage(`${missingKeys.join(', ')} is Required`));
-            }
-
-            const oldImgResult = await pool.request().query(`
-                SELECT Img FROM SponsorMaster WHERE SponsorUkeyId = '${SponsorUkeyId}';
-            `);
-            const oldImg = oldImgResult.recordset?.[0]?.Img;
 
             const deleteResult = await pool.request().query(deleteQuery);
             const insertResult = await pool.request().query(insertQuery);
 
             if (deleteResult.rowsAffected[0] === 0 && insertResult.rowsAffected[0] === 0) {
-                if (Img) deleteImage(req?.files?.Img?.[0]?.path); // Only delete if `Img` exists
                 return res.status(400).json({ ...errorMessage('No Sponsor Master Updated.') });
             }
 
-            if (oldImg && req.files && req.files.Img && req.files.Img.length > 0) deleteImage('./media/Sponsor/' + oldImg); // Only delete old image if it exists
             return res.status(200).json({ 
                 ...successMessage('New Sponsor Master Updated Successfully.'), 
-                ...req.body, SponsorUkeyId, Img 
+                ...req.body 
             });
 
         } else {
-            if (Img) deleteImage(req?.files?.Img?.[0]?.path); // Only delete if `Img` exists
             return res.status(400).json({
                 ...errorMessage("Use 'A' flag to Add and 'U' flag to update, it is compulsory to send flag.")
             });
         }
     } catch (error) {
-        if (Img) deleteImage(req?.files?.Img?.[0]?.path); // Only delete if `Img` exists
         if (flag === 'A') {
             console.log('Add Sponsor Master Error :', error);
         }
@@ -120,24 +100,15 @@ const SponsorMaster = async (req, res) => {
 
 const RemoveSponsor = async (req, res) => {
     try {
-        const { SponsorUkeyId } = req.query;
+        const { SponsorUkeyId, OrganizerUkeyId } = req.query;
 
-        // Check if required keys are missing
-        const missingKeys = checkKeysAndRequireValues(['SponsorUkeyId'], req.query);
+        const missingKeys = checkKeysAndRequireValues(['SponsorUkeyId', 'OrganizerUkeyId'], req.query);
         if (missingKeys.length > 0) {
             return res.status(400).json(errorMessage(`${missingKeys.join(', ')} is Required`));
         }
 
-        // Fetch the old image path before deleting the record
-        const oldImgResult = await pool.request().query(`
-            SELECT Img FROM SponsorMaster WHERE SponsorUkeyId = '${SponsorUkeyId}';
-        `);
-
-        const oldImg = oldImgResult.recordset?.[0]?.Img; // Safely access the first record
-
-        // Execute the DELETE query
         const deleteQuery = `
-            DELETE FROM SponsorMaster WHERE SponsorUkeyId = '${SponsorUkeyId}';
+            DELETE FROM SponsorMaster WHERE SponsorUkeyId = ${setSQLStringValue(SponsorUkeyId)} AND OrganizerUkeyId = ${setSQLStringValue(OrganizerUkeyId)};
         `;
         const deleteResult = await pool.request().query(deleteQuery);
 
@@ -145,12 +116,6 @@ const RemoveSponsor = async (req, res) => {
             return res.status(400).json({ ...errorMessage('No Sponsor Master Deleted.') });
         }
 
-        // Delete the old image if it exists
-        if (oldImg) {
-            deleteImage('./media/Sponsor/' + oldImg);
-        }
-
-        // Return success response
         return res.status(200).json({ ...successMessage('Sponsor Master Deleted Successfully.'), SponsorUkeyId });
     } catch (error) {
         console.log('Delete Sponsor Master Error :', error);
