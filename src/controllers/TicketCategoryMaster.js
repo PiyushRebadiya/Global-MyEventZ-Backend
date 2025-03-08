e2e062b4ff02466ae94f0abf74414c667d85d79c
parth chothani <parthchothani560@gmail.com>
2025-03-08 11:35:28 +0530

    add ConvenienceFee column in ticket category masyter

--- src/controllers/TicketCategoryMaster.js
+++ src/controllers/TicketCategoryMaster.js
@@ -1,4 +1,4 @@
-const { errorMessage, successMessage, checkKeysAndRequireValues, generateCODE, setSQLBooleanValue, getCommonKeys, generateJWTT, generateUUID, getCommonAPIResponse, toFloat, setSQLStringValue } = require("../common/main");
+const { errorMessage, successMessage, checkKeysAndRequireValues, generateCODE, setSQLBooleanValue, getCommonKeys, generateJWTT, generateUUID, getCommonAPIResponse, toFloat, setSQLStringValue, setSQLDecimalValue } = require("../common/main");
 const {pool} = require('../sql/connectToDatabase');
 
 const FetchTicketCategory = async(req, res)=>{
@@ -34,7 +34,7 @@ const FetchTicketCategory = async(req, res)=>{
 }
 
 const TicketCategoryMaster = async(req, res)=>{
-    const { TicketCateUkeyId, TicketLimits, Category, TicketPrice, IsActive = true, OrganizerUkeyId, EventUkeyId, DiscPer, DiscAmt, flag} = req.body;
+    const { TicketCateUkeyId, TicketLimits, Category, TicketPrice, IsActive = true, OrganizerUkeyId, EventUkeyId, DiscPer, DiscAmt, flag, ConvenienceFee} = req.body;
     const {IPAddress, ServerName, EntryTime} = getCommonKeys(req);
     try{
         const missingKeys = checkKeysAndRequireValues(['TicketCateUkeyId', 'TicketLimits', 'Category', 'TicketPrice', 'IsActive', 'OrganizerUkeyId', 'EventUkeyId'], req.body)
@@ -43,9 +43,9 @@ const TicketCategoryMaster = async(req, res)=>{
         }
         const insertQuery = `
             INSERT INTO TicketCategoryMaster (
-                TicketCateUkeyId, TicketLimits, Category, TicketPrice, IsActive, OrganizerUkeyId, EventUkeyId, flag, IpAddress, HostName, EntryDate, DiscPer, DiscAmt
+                TicketCateUkeyId, TicketLimits, Category, TicketPrice, IsActive, OrganizerUkeyId, EventUkeyId, flag, IpAddress, HostName, EntryDate, DiscPer, DiscAmt, ConvenienceFee
             ) VALUES (
-                ${setSQLStringValue(TicketCateUkeyId)}, ${setSQLStringValue(TicketLimits)}, ${setSQLStringValue(Category)}, ${setSQLStringValue(TicketPrice)}, ${setSQLStringValue(IsActive)}, ${setSQLStringValue(OrganizerUkeyId)}, ${setSQLStringValue(EventUkeyId)}, ${setSQLStringValue(flag)}, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, ${setSQLStringValue(DiscPer)}, ${setSQLStringValue(DiscAmt)}
+                ${setSQLStringValue(TicketCateUkeyId)}, ${setSQLStringValue(TicketLimits)}, ${setSQLStringValue(Category)}, ${setSQLStringValue(TicketPrice)}, ${setSQLStringValue(IsActive)}, ${setSQLStringValue(OrganizerUkeyId)}, ${setSQLStringValue(EventUkeyId)}, ${setSQLStringValue(flag)}, ${setSQLStringValue(IPAddress)}, ${setSQLStringValue(ServerName)}, ${setSQLStringValue(EntryTime)}, ${setSQLStringValue(DiscPer)}, ${setSQLStringValue(DiscAmt)}, ${setSQLDecimalValue(ConvenienceFee)}
             );
         `
         const deleteQuery = `
